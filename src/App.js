51ea79e843da1fdc859f1d1aa09e0db8d8ae5be9
AnthonyTB <anthonylol@protonmail.com>
2020-04-18 20:44:52 -0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,144 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useReducer, useEffect, useCallback } from 'react';
 import './App.css';
+import Node from './Node';
 
 function App() {
+  const Reducer = (prevState, { type, payload }) => {
+    switch (type) {
+      case 'start':
+        return {
+          ...prevState,
+          start: payload.start,
+        };
+      case 'visited':
+        return {
+          ...prevState,
+          visited: payload.visited,
+        };
+      case 'grid':
+        return {
+          ...prevState,
+          grid: payload.grid,
+        };
+      case 'isPregame':
+        return {
+          ...prevState,
+          isPregame: payload.isPregame,
+        };
+      default:
+        return '';
+    }
+  };
+
+  const [state, dispatch] = useReducer(Reducer, {
+    start: { row: 2, col: 10 },
+    visited: [],
+    grid: null,
+    isPregame: true,
+  });
+
+  const dataSetter = (section, data) => {
+    dispatch({
+      type: `${section}`,
+      payload: {
+        [section]: data,
+      },
+    });
+  };
+
+  const createNode = useCallback(
+    (col, row) => {
+      return {
+        col,
+        row,
+        isStart: row === state.start.row && col === state.start.col,
+        isVisited: () =>
+          state.visited.length > 0
+            ? state.visited.forEach((tile) =>
+                row === tile.newRow && col === tile.newCol ? true : false
+              )
+            : false,
+      };
+    },
+    [state.start.col, state.start.row, state.visited]
+  );
+
+  useEffect(() => {
+    const getInitialGrid = () => {
+      const grid = [];
+      for (let row = 0; row < 10; row++) {
+        const currentRow = [];
+        for (let col = 0; col < 19; col++) {
+          currentRow.push(createNode(col, row));
+        }
+        grid.push(currentRow);
+      }
+      return grid;
+    };
+    dataSetter('grid', getInitialGrid());
+  }, [createNode]);
+
+  const handleClick = (ev) => {
+    ev.preventDefault();
+    const node = ev.target.id.split('-');
+    const newRow = Number(node[1]);
+    const newCol = Number(node[2]);
+    if (state.isPregame) {
+      dataSetter('start', { row: newRow, col: newCol });
+    } else {
+      const newArr = state.visited.concat({ newRow, newCol });
+      dataSetter('visited', newArr);
+      dataSetter('start', { row: newRow, col: newCol });
+    }
+  };
+
+  const startGame = () => {
+    dataSetter('isPregame', false);
+    console.log('Starting...');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <header className='App-header'>Acid Path Trainer</header>
+      {state.grid ? (
+        <section className='grid'>
+          {state.isPregame ? (
+            <button type='button' onClick={startGame}>
+              Start
+            </button>
+          ) : (
+            ''
+          )}
+          <h2>
+            {state.isPregame
+              ? 'Choose a starting position then click start'
+              : 'Dodge the acid'}
+          </h2>
+          {state.grid.map((row, rowIdx) => {
+            return (
+              <div key={rowIdx}>
+                {row.map((node, nodeIdx) => {
+                  const { row, col, isStart, isVisited } = node;
+                  return (
+                    <button key={rowIdx} onClick={handleClick} type='button'>
+                      <Node
+                        key={nodeIdx}
+                        col={col}
+                        isStart={isStart}
+                        isVisited={isVisited}
+                        row={row}
+                      ></Node>
+                    </button>
+                  );
+                })}
+              </div>
+            );
+          })}
+        </section>
+      ) : (
+        'Loading...'
+      )}
+    </>
   );
 }
 
---
+++ src/Node.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Node.css';
+
+export default function Node(props) {
+  const { col, isVisited, row } = props;
+  console.log(isVisited());
+  const visitStatus = isVisited() ? 'visited' : 'unvisited';
+  const startStatus = props.isStart ? 'start' : '';
+
+  return (
+    <div
+      id={`node-${row}-${col}`}
+      className={`node ${visitStatus} ${startStatus}`}
+    ></div>
+  );
+}
